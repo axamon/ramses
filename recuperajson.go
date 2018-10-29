0e87d785f10b780927d1ba41443979bd12fc273b
Alberto Bregliano <alberto.bregliano@telecomitalia.it>
2018-10-29 20:36:56 +0100

    Ramses regna!

--- recuperajson.go
+++ recuperajson.go
@@ -71,7 +71,7 @@ func recuperavariabile(variabile string) (result string, err error) {
 	if result, ok := os.LookupEnv(variabile); ok && len(result) != 0 {
 		return result, nil
 	}
-	err = fmt.Errorf("la variabile %s non esiste o Ã¨ vuota", variabile)
+	err = fmt.Errorf("la variabile %s non esiste o è vuota", variabile)
 	fmt.Fprintln(os.Stderr, err.Error())
 	return
 }
