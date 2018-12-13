6c282187649406c0970558cbfedeb2c2f6ed8ba4
Alberto Bregliano <alberto.bregliano@pm.me>
2018-12-13 03:09:43 +0100

    ok

---
+++ bot/mandamail.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	gomail "gopkg.in/gomail.v2"
+)
+
+func mandamail(from, to, device string) {
+
+	subject := "Allarme ppp su " + device
+	body := "Ciao <b>Gringo</b> <hr> rilevato abbassamento anomalo sessioni ppp su " + device
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetAddressHeader("Cc", "alberto.bregliano@telecomitalia.it", "Alberto Bregliano")
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+	//m.Attach("/home/Alex/lolcat.jpg")
+
+	d := gomail.NewDialer("smtp.gmail.com", 587, "alberto.bregliano@gmail.com", "lklfldsfrzlmcobs")
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		log.Printf("impossibile inviare mail %s\n", err.Error())
+	}
+}
