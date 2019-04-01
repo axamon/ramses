17b296600868361d7b1c0bba182fbb413df1ae8e
Alberto Bregliano <alberto.bregliano@pm.me>
2019-04-02 00:01:31 +0200

    elabora serie

---
+++ estraiSerie.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+)
+
+func estraiSerie(result []interface{}) (serieppptime, seriepppvalue []float64) {
+	// Estraggo serie dati dal risultato query http
+	d := result[0].(map[string]interface{})
+	dp := d["dps"].(map[string]interface{})
+
+	// Mette i timestamps in ordine
+	tempi := make([]string, 0)
+	for t := range dp {
+		tempi = append(tempi, t)
+	}
+	// Ordina i timestamps in maniera crescente
+	sort.Strings(tempi)
+
+	// Crea variabili di appoggio
+	//var seriepppvalue []float64
+	//var serieppptime []float64
+
+	// Cicla i tempi
+	for _, t := range tempi {
+		tint, _ := strconv.Atoi(t)
+		serieppptime = append(serieppptime, float64(tint))
+		seriepppvalue = append(seriepppvalue, dp[t].(float64))
+		//fmt.Println("orario: ", t, "valore: ", dp[t])
+	}
+
+	return serieppptime, seriepppvalue
+}
